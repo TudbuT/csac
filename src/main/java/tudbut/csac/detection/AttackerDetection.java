d1d1e5227bed4e592a5f084433943e3e15cc7791
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-07 14:24:01 +0100

    probably working now

--- src/main/java/tudbut/csac/detection/AttackerDetection.java
+++ src/main/java/tudbut/csac/detection/AttackerDetection.java
@@ -8,16 +8,15 @@ public class AttackerDetection {
     static Minecraft mc = Minecraft.getMinecraft();
     
     public static void onHit(EntityLivingBase entity) {
-        if(entity.getEntityId() != mc.player.getEntityId())
-            AntiCheat.onHit(
-                    entity,
-                    mc.world.getEntities(
-                            EntityLivingBase.class,
-                            e ->
-                                    e.getDistance(entity) < 10 &&
-                                    e.getEntityId() != mc.player.getEntityId() &&
-                                    e.getEntityId() != entity.getEntityId()
-                    ).toArray(new EntityLivingBase[0])
-            );
+        AntiCheat.onHit(
+                entity,
+                mc.world.getEntities(
+                        EntityLivingBase.class,
+                        e ->
+                                e.getDistance(entity) < 10 &&
+                                e.getEntityId() != mc.player.getEntityId() &&
+                                e.getEntityId() != entity.getEntityId()
+                ).toArray(new EntityLivingBase[0])
+        );
     }
 }
