583db9be3195c1feb601e58b12a9d41f7ceb84bf
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-07 17:49:19 +0100

    reach more aggressive

--- src/main/java/tudbut/csac/anticheat/check/ReachCheck.java
+++ src/main/java/tudbut/csac/anticheat/check/ReachCheck.java
@@ -77,7 +77,7 @@ public class ReachCheck extends Check {
         
         // Reach below configured max?
         public boolean isReachNormal() {
-            return playerReach < 4.5;
+            return playerReach < 4;
         }
         
         // Offense counter for plugin
