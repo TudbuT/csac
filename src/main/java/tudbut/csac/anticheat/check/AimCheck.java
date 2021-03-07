84002d25dc1b0a085b80c21f2a4571bbd0cbca42
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-07 14:35:03 +0100

    aimcheck working

--- src/main/java/tudbut/csac/anticheat/check/AimCheck.java
+++ src/main/java/tudbut/csac/anticheat/check/AimCheck.java
@@ -9,7 +9,7 @@ public class AimCheck extends Check {
         float f = 0;
     
         float aimDist = AntiCheat.aimDist(attacked, attacker);
-        f += aimDist / 360;
+        f += aimDist / (180);
         
         return f;
     }
