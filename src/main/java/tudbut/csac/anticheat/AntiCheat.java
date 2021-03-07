5d265c7838aabde86390cb2637e4badbbc69059a
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-07 14:29:16 +0100

    aimcheck working

--- src/main/java/tudbut/csac/anticheat/AntiCheat.java
+++ src/main/java/tudbut/csac/anticheat/AntiCheat.java
@@ -34,8 +34,12 @@ public class AntiCheat {
     
     public static float aimDist(EntityLivingBase attacked, EntityLivingBase attacker)  {
         Vector2d vector2d = Utils.getLegitRotations(attacked.getPositionVector().add(0, 1.5, 0));
-        float distX = (float) (vector2d.getX() - attacker.rotationYaw % 360);
-        float distY = (float) (vector2d.getY() - attacker.rotationPitch % 360);
+        float distX0 = (float) (vector2d.getX() - attacker.rotationYaw % 360);
+        float distY0 = (float) (vector2d.getY() - attacker.rotationPitch % 360);
+        float distX1 = (float) (MathHelper.wrapDegrees(vector2d.getX()) - MathHelper.wrapDegrees(attacker.rotationYaw));
+        float distY1 = (float) (MathHelper.wrapDegrees(vector2d.getY()) - MathHelper.wrapDegrees(attacker.rotationPitch));
+        float distX = Math.min(Math.abs(distX0), Math.abs(distX1));
+        float distY = Math.min(Math.abs(distY0), Math.abs(distY1));
         return (float) Math.sqrt(distX * distX + distY * distY);
     }
     
