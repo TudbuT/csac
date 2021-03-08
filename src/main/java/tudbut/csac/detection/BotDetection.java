158ab046c895a16d77005c72a6d02eeb2a3dfd24
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-08 09:56:06 +0100

    bug fix

--- src/main/java/tudbut/csac/detection/BotDetection.java
+++ src/main/java/tudbut/csac/detection/BotDetection.java
@@ -28,8 +28,8 @@ public class BotDetection {
     public static void onTick() {
         EntityLivingBase[] entities = mc.world.getEntities(EntityLivingBase.class, e -> true).toArray(new EntityLivingBase[0]);
         for (int i = 0; i < entities.length; i++) {
+            ticksExisted.setIfNull(entities[i].getEntityId(), 0);
             if(!entities[i].isInvisible() && entities[i].getHeldItemMainhand().getItem() != Items.AIR) {
-                ticksExisted.setIfNull(entities[i].getEntityId(), 0);
                 ticksExisted.set(entities[i].getEntityId(), ticksExisted.get(entities[i].getEntityId()) + 1);
             }
         }
