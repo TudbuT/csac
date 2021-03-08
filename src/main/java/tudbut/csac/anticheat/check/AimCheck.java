a1a67fe0c32055e040c1246e43ada367712c52d4
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-03-08 22:07:58 +0100

    AimCheck more aggressive

--- src/main/java/tudbut/csac/anticheat/check/AimCheck.java
+++ src/main/java/tudbut/csac/anticheat/check/AimCheck.java
@@ -12,11 +12,11 @@ public class AimCheck extends Check {
         float f = 0;
     
         float aimDist = AntiCheat.aimDist(attacked, attacker);
-        f += aimDist / 270;
+        f += aimDist / 360;
         
         rotationsLastTick.setIfNull(attacker.getEntityId(), new Vector2d(attacker.rotationYaw, attacker.rotationPitch));
         Vector2d rot = rotationsLastTick.get(attacker.getEntityId());
-        f += AntiCheat.aimDist((float) rot.getX(), (float) rot.getY(), attacker.rotationYaw, attacker.rotationPitch) / 360 * 0.75;
+        f += AntiCheat.aimDist((float) rot.getX(), (float) rot.getY(), attacker.rotationYaw, attacker.rotationPitch) / 360 * 0.5;
         rotationsLastTick.set(attacker.getEntityId(), new Vector2d(attacker.rotationYaw, attacker.rotationPitch));
     
         f *= 0.5;
